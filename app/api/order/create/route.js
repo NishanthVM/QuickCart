7ab26176410c4612b66c9e053341eb34e2017c17
NishanthVM <nishanthvm94@gmail.com>
2025-09-02 20:29:17 +0530

    payment

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -26,6 +26,7 @@ export async function POST(request) {
         items,
         amount: amount + Math.floor(amount * 0.02),
         date: Date.now(),
+        paymentType: "COD",
       },
     });
 
